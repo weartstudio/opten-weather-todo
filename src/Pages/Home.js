aaf1a1ff882db5e7d2fdef063bec1c4d080f7dff
weartstudio <balazs@weart.hu>
2022-09-09 12:58:27 +0200

    vessző

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
 	return (
 		<div className='text-center text-white'>
-			<h2>Üdözöllek kérlek válassz projektet</h2>
+			<h2>Üdözöllek, kérlek válassz projektet!</h2>
 			<a href="/todo" className="btn btn-primary px-4 m-3">Todo</a>
 			<a href="/city" className="btn btn-primary px-4  m-3">City</a>
 		</div>
