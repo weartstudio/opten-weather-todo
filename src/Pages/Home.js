5103dd9bb102e380dddfefd28843ed8de13b2822
weartstudio <balazs@weart.hu>
2022-09-09 13:20:52 +0200

    github

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,6 +6,7 @@ function Home() {
 			<h2>Üdözöllek, kérlek válassz projektet!</h2>
 			<a href="/todo" className="btn btn-primary px-4 m-3">Todo</a>
 			<a href="/city" className="btn btn-primary px-4  m-3">City</a>
+			<a href="https://github.com/weartstudio/opten-weather-todo" className="btn btn-primary px-4  m-3"><i class="fa-brands fa-github"></i></a>
 		</div>
 	)
 }
